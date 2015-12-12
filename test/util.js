5da9de1849ee24b63dd663ed4947133ff56e442c
yuanwen <wenshin2011@gmail.com>
2015-12-13 01:18:24 +0800

    添加支持指定属性浅拷贝

--- test/util.js
+++ test/util.js
@@ -98,35 +98,50 @@ describe('util.putEntireFlatedObj', function () {
 });
 
 describe('util.FlatMap', function () {
+  let src = {a: 1, b: {c: {d: 1}, e: {g: 1}}};
+
   it('should get value deep cloned default, shallow clone with isDeepClone is false', function () {
-    let src = {a: 1, b: {c: {d: 1}}};
     let flated = new FlatMap(src);
-    assert.notEqual(src.b, flated.get('b'));
-    assert.deepEqual(src.b, flated.get('b'));
-    assert.equal(src.b.c, flated.get('b', false).c);
+    let deepB = flated.get('b');
+    let shallowB = flated.get('b', false);
+    assert.notEqual(src.b, deepB);
+    assert.deepEqual(src.b, deepB);
+    assert.equal(src.b.c, shallowB.c);
+    let deepBC = flated.get('b', new Set(['b.c']));
+    // 浅拷贝指定的属性
+    assert.equal(src.b.c, deepBC.c);
+    // 深拷贝其他属性
+    assert.notEqual(src.b.e, deepBC.e);
+    assert.deepEqual(src.b.e, deepBC.e);
   });
 
-  it('should FlatMap.getSrc() deep cloned default, shallow clone with isDeepClone is false', function () {
-    let src = {a: 1, b: {c: {d: 1}}};
+  it('should FlatMap.getSrc() deep cloned default, shallow clone with shallowClonePropSet', function () {
     let flated = new FlatMap(src);
     let deepSrc = flated.getSrc();
+    let shallowB = flated.get('b', false);
     let shallowSrc = flated.getSrc(false);
-    assert.notEqual(deepSrc.b.c, flated.get('b', false).c);
-    assert.deepEqual(deepSrc.b, flated.get('b', false));
-    assert.equal(shallowSrc.b.c, flated.get('b', false).c);
-    assert.deepEqual(shallowSrc.b, flated.get('b', false));
+    assert.notEqual(deepSrc.b.c, shallowB.c);
+    assert.deepEqual(deepSrc.b, shallowB);
+    assert.equal(shallowSrc.b.c, shallowB.c);
+
+    let deepBC = flated.get('b', new Set(['b.c']));
+    // 浅拷贝指定的属性
+    assert.equal(shallowSrc.b.c, deepBC.c);
+    assert.notEqual(shallowSrc.b.e, deepBC.e);
+    assert.deepEqual(shallowSrc.b.e, deepBC.e);
   });
 
   it('should change FlatMap.getSrc() value when patch or put', function () {
     let triggered = [];
+    let shallowSet = new Set('b');
     let flated = new FlatMap({a: 1, b: {c: 2}});
     flated.patch({b: {d: 1}}, (query, value) => {
       triggered.push([query, value]);
     });
     assert.ok(triggered.length);
     assert.deepEqual([['b.d', 1], ['b', {c: 2, d: 1}]], triggered);
-    assert.deepEqual(flated.getSrc(false).b, flated.get('b', false));
-    assert.deepEqual(flated.getSrc().b, flated.get('b', false));
+    assert.deepEqual(flated.getSrc(shallowSet).b, flated.get('b', shallowSet));
+    assert.deepEqual(flated.getSrc().b, flated.get('b', shallowSet));
 
     triggered = [];
     flated.put('b', {e: 1}, (query, value) => {
@@ -138,7 +153,7 @@ describe('util.FlatMap', function () {
       ['b.c', undefined],
       ['b.d', undefined],
       ['b.e', 1]], triggered);
-    assert.deepEqual(flated.getSrc(false).b, flated.get('b', false));
+    assert.deepEqual(flated.getSrc(shallowSet).b, flated.get('b', shallowSet));
   });
 
   it('should change all when put entirely', function () {
@@ -154,6 +169,6 @@ describe('util.FlatMap', function () {
       ['b.c', undefined],
       ['b.e', 1],
       ['b', cover.b]], triggered);
-    assert.deepEqual(flated.getSrc(false), flated.get(false));
+    assert.deepEqual(flated.getSrc(null, false), flated.get('', null, false));
   });
 });
