761b65713c34b30e425f330dba94d77df226896b
yuanwen <wenshin2011@gmail.com>
2015-12-13 17:38:29 +0800

    store 基础功能

--- test/store.js
+++ test/store.js
@@ -4,28 +4,28 @@ import Store from '../lib/store';
 
 describe('store', function () {
 
-  it('should not set values directly!', function () {
+  it('should not put values directly!', function () {
     let store = new Store({a: 1, b: {c: 1}});
-    assert.throws(() => store.set('a', 2), StoreWritePermissionError);
+    assert.throws(() => store.put('a', 2), StoreWritePermissionError);
 
     let action = {};
     store.registerWriter(action);
-    assert.doesNotThrow(() => store.set('a', 2, action), StoreWritePermissionError);
+    assert.doesNotThrow(() => store.put('a', 2, action), StoreWritePermissionError);
   });
 
-  it('should trigger root level property change event!', function () {
+  it('should trigger root level property change event!', function (done) {
     let store = new Store({a: 1, b: {c: 1}});
-    let changedA = false;
-    store.onchange('a', function(old, changed) {
-      changedA = true;
-      assert.notDeepEqual(old, changed, 'When changed the "old" and "changed" value must not deep equal');
+    let triggerValues = [1, 2];
+    store.onChange('a', function(value) {
+      assert.equal(triggerValues.shift(), value);
     });
-    assert(changedA, 'changed handler will trigger immediately when change event');
 
-    changedA = false;
     let action = {get: () => {}};
     store.registerWriter(action);
-    store.set('a', 2, action);
-    assert(changedA, 'changed handler will trigger when values changed');
+    store.put('a', 2, action);
+    setTimeout(() => {
+      assert.ok(!triggerValues.length, `值为“${triggerValues}”的事件没有触发`);
+      done();
+    }, 25);
   });
 });
