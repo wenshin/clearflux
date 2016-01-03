99a7fbf0909992b91f0d409078b05f05581255b9
yuanwen <wenshin2011@gmail.com>
2016-01-03 15:40:57 +0800

    async pipeline

--- test/utils/pipeline.js
+++ test/utils/pipeline.js
@@ -1,5 +1,43 @@
 import {assert} from 'chai';
-import Pipeline from '../../lib/utils/pipeline';
+import Pipeline, {pipelineWrap} from '../../lib/utils/pipeline';
+
+describe('pipelineWrap', function () {
+  it('应该正确运行非异步方法', function () {
+    let except = pipelineWrap(10)
+      .flow(v => -v)
+      .flow(v => 1/v)
+      .finish();
+    assert.equal(except, -0.1);
+  });
+
+  it('应该正确运行异步方法', function (done) {
+    let asyncPipe = data => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(data * 2);
+        }, 10);
+      });
+    };
+    let promise = pipelineWrap(10)
+      .flow(v => -v)
+      .flowAsync(asyncPipe)
+      .flow(v => 1/v)
+      .flowAsync(asyncPipe)
+      .flow(v => v * 3)
+      .finish();
+    assert.ok(promise instanceof Promise, '异步结果返回 Promise 对象');
+
+    let except;
+    promise
+      .then(data => {
+        except = data;
+      });
+    setTimeout(() => {
+      assert.equal(except, 1/-20*2*3);
+      done();
+    }, 30);
+  });
+});
 
 describe('Pipeline', function () {
   it('应该正确运行委托自 Array 的方法', function () {
