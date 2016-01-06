3ee9e9133fd5f3c7a7fd48a7164915b68938477e
yuanwen <wenshin2011@gmail.com>
2016-01-06 16:56:13 +0800

    change mixins to middlewares

--- test/pipeline/pipelineWrap.js
+++ test/pipeline/pipelineWrap.js
@@ -1,10 +1,10 @@
 import {assert} from 'chai';
 import {pipelineWrap} from '../../lib/pipeline';
-import {makeRoundNumberMixin, toNumberMixin} from '../../lib/pipeline/mixins/number';
+import {makeRoundNumberMiddleware, toNumberMiddleware} from '../../lib/pipeline/middlewares/number';
 
 describe('pipelineWrap', function () {
   it('应该正确运行非异步方法', function () {
-    let except = pipelineWrap('10', {mixins: [toNumberMixin]})
+    let except = pipelineWrap('10', {middlewares: [toNumberMiddleware]})
       .flow(v => {
         if (typeof v !== 'number') throw new TypeError('not a number');
         return v;
@@ -29,7 +29,7 @@ describe('pipelineWrap', function () {
       .flow(v => 1/v)
       .flowAsync(asyncPipe)
       .flow(v => v * 3)
-      .finish({mixins: [makeRoundNumberMixin()]});
+      .finish({middlewares: [makeRoundNumberMiddleware()]});
     assert.ok(promise instanceof Promise, '异步结果返回 Promise 对象');
 
     let except;
@@ -52,7 +52,7 @@ describe('pipelineWrap', function () {
       .reduceFlow({
         pipe: (pre, cur) => pre + cur,
         initialValue: 0,
-        postMixins: [makeRoundNumberMixin()]
+        postMiddlewares: [makeRoundNumberMiddleware()]
       })
       .finish();
     assert.equal(except, 0.15);
