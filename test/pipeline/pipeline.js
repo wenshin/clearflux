1ccf966f827af6ccfa411778973f9b131f1e4295
yuanwen <wenshin2011@gmail.com>
2016-01-15 10:26:44 +0800

    添加 pipeline logger

--- test/pipeline/pipeline.js
+++ test/pipeline/pipeline.js
@@ -1,5 +1,6 @@
 import {assert} from 'chai';
 import Pipeline from '../../lib/pipeline';
+import PipelineLogger from '../../lib/pipeline/middlewares/logger';
 
 describe('Pipeline', function () {
   it('应该正确运行委托自 Array 的方法', function () {
@@ -17,4 +18,18 @@ describe('Pipeline', function () {
 
     assert.equal(pipeline.flow(10), -0.1);
   });
+
+  it('应该正确运行 Logger 中间件', function () {
+    let pipeline = new Pipeline(
+      'Pipeline Logger test',
+      [
+        {name: 'pipe1', handle: v => v},
+        {name: 'Negative', handle: v => -v}
+      ]
+    );
+    Pipeline.pipelineMiddlewares.push(PipelineLogger);
+    assert.equal(pipeline.flow(10), -10);
+    Pipeline.pipelineMiddlewares = [];
+  });
+
 });
