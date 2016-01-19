0791710be923512fd5dc7639e7f93635b450ece4
yuanwen <wenshin2011@gmail.com>
2016-01-19 11:03:18 +0800

    fix combine bug

--- test/store.js
+++ test/store.js
@@ -213,24 +213,25 @@ describe('store', function () {
 
 describe('store:combine', function () {
   let action = {name: 'combineAction'};
-  let store = new Store({a: 1, b: 2});
-  store.combine({
-    ab: {queries: ['a', 'b'], get: (a, b) => a + b},
-    ab1: {queries: ['a', 'b'], get: (a, b) => a - b}
+  let store;
+
+  let triggerValues = [];
+  let handleChange = v => triggerValues.push(v);
+
+  beforeEach(() => {
+    store = new Store({a: 1, b: 2});
+    store.combine({
+      ab: {queries: ['a', 'b'], get: (a, b) => a + b},
+      ab1: {queries: ['a', 'b'], get: (a, b) => a - b}
+    });
+    store.registerWriter(action);
+    triggerValues = [];
   });
-  store.registerWriter(action);
 
   it('should combine props to one and can trigger change event', done => {
-
-    let triggerValues = [];
-    store.onChange('ab', ab => {
-      triggerValues.push(ab);
-    });
+    store.onChange('ab', handleChange);
 
     store.put('a', 3, action);
-
-    // store.startLoading('b');
-    // store.setErrors('b', 'error');
 
     setTimeout(() => {
       assert.ok(triggerValues.length, `没有触发事件`);
@@ -238,8 +239,74 @@ describe('store:combine', function () {
         {value: 3, loading: false, errors: []},
         {value: 5, loading: false, errors: []}
       ]);
+      store.offChange('ab', handleChange);
       done();
     }, 20);
+  });
+
+  it('should trigger loading event right', done => {
+    store.onChange('ab1', handleChange);
+
+    store.startLoading('a', action);
+
+    setTimeout(() => {
+      store.stopLoading('a', action);
+    }, 20);
+
+    setTimeout(() => {
+      assert.ok(triggerValues.length, `没有触发事件`);
+      assert.deepEqual(triggerValues, [
+        {value: -1, loading: false, errors: []},
+        {value: -1, loading: true, errors: []},
+        {value: -1, loading: false, errors: []}
+      ]);
+      store.offChange('ab1', handleChange);
+      done();
+    }, 40);
+  });
+
+  it('should trigger errors event right', done => {
+    store.onChange('ab1', v => handleChange(v));
+
+    store.setErrors('a', 'error', action);
+
+    setTimeout(() => {
+      store.removeErrors('a', action);
+    }, 20);
+
+    setTimeout(() => {
+      assert.ok(triggerValues.length, `没有触发事件`);
+      assert.deepEqual(triggerValues, [
+        {value: -1, loading: false, errors: []},
+        {value: -1, loading: false, errors: ['error']},
+        {value: -1, loading: false, errors: []}
+      ]);
+      store.offChange('ab1', handleChange);
+      done();
+    }, 40);
+  });
+
+  it('should trigger change events when mix operation', done => {
+    store.onChange('ab1', v => handleChange(v));
+
+    store.put('a', 3, action);
+    store.startLoading('a', action);
+
+    setTimeout(() => {
+      store.stopLoading('a', action);
+      store.setErrors('b', 'error', action);
+    }, 20);
+
+    setTimeout(() => {
+      assert.ok(triggerValues.length, `没有触发事件`);
+      assert.deepEqual(triggerValues, [
+        {value: -1, loading: false, errors: []},
+        {value: 1, loading: true, errors: []},
+        {value: 1, loading: false, errors: ['error']}
+      ]);
+      store.offChange('ab1', handleChange);
+      done();
+    }, 40);
   });
 });
 
