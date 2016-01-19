625b7cd8b643ab70913decc2a8b241d3f2ea57f4
yuanwen <wenshin2011@gmail.com>
2016-01-19 11:56:16 +0800

    change Action to Actions

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 import {assert} from 'chai';
 import clearflux, {
-  Store, Action, Validator, ValidatorMap, StoreWritePermissionError
+  Store, Actions, Validator, ValidatorMap, StoreWritePermissionError
 } from '../lib';
 
 describe('clearflux', function () {
   it('should have all props!', function () {
     assert.ok(clearflux.createStore);
-    assert.ok(clearflux.createAction);
+    assert.ok(clearflux.createActions);
     assert.ok(Store);
-    assert.ok(Action);
+    assert.ok(Actions);
     assert.ok(Validator);
     assert.ok(ValidatorMap);
     assert.ok(StoreWritePermissionError);
