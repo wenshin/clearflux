2c5fe5ce6534f959dc9365d0decceaee601ef0ef
yuanwen <wenshin2011@gmail.com>
2016-05-02 13:51:54 +0800

    Update: lightdb add delete api

--- test/lightdb.js
+++ test/lightdb.js
@@ -7,9 +7,21 @@ describe('LightDB', function () {
     assert.deepEqual(db.findById(1), {id: 1, name: 'name1'});
   });
 
+  it('should findByIndex right', function () {
+    let db = new LightDB([{id: 1, name: 'name1'}, {id: 2, name: 'name2'}]);
+    assert.deepEqual(db.findByIndex(0), {id: 1, name: 'name1'});
+  });
+
   it('should findByFieldName right', function () {
     let db = new LightDB([{id: 1, name: 'name1'}, {id: 2, name: 'name2'}]);
-    assert.deepEqual(db.findByFieldName('name2', 'name'), {id: 2, name: 'name2'});
+    assert.deepEqual(db.findByFieldName('name', 'name2'), {id: 2, name: 'name2'});
+  });
+
+  it('should delByIndex right', function () {
+    let db = new LightDB([{id: 1, name: 'name1'}, {id: 2, name: 'name2'}]);
+    assert.deepEqual(db.delByIndex(0), {id: 1, name: 'name1'});
+    assert.deepEqual(db.length, 1);
+    assert.deepEqual(db.findByIndex(0), {id: 2, name: 'name2'});
   });
 
   it('should inherited Array methods!', function () {
@@ -37,6 +49,8 @@ describe('LightDB', function () {
     assert.deepEqual(db.map(v => v.id), [1, 2]);
     assert.deepEqual(db.reduce((prev, cur) => prev + cur.id, 0), 3);
     assert.deepEqual(db.filter(v => v.id === 2), [{id: 2, name: 'name2'}]);
+    assert.deepEqual(db.some(v => v.id === 2), true);
+    assert.deepEqual(db.every(v => v.id === 2), false);
   });
 });
 
